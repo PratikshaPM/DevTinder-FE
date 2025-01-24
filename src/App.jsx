cc63db12d78e9f2e51f78e2fed081bfd5f3e3774
PratikshaPM <pratikshamahajan2@gmail.com>
2025-01-24 13:16:57 +0530

    Configuration done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainContainer from "./components/MainContainer";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<MainContainer />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
